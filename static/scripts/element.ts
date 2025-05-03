7b4f3303985e2b7840d5a18aeae9f80603cad95b
Christos Froussios <Froussios@users.noreply.github.com>
2025-05-03 04:32:57 +0000

    Fix: Use data response in the example page #1

--- static/scripts/element.ts
+++ static/scripts/element.ts
@@ -8,9 +8,30 @@ export class RootElement extends LitElement {
         super();
     }
 
+    @property({ type: Object }) data = {};
+
+    async firstUpdated() {
+        try {
+            const response = await fetch("./data/");
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            this.data = await response.json();
+            console.log('data', JSON.stringify(this.data));
+        } catch (error) {
+            console.error("Failed to fetch data:", error);
+        }
+    }
+
     render() {
         return html`
-        <div>All set</div>
+        <div>Rendered successfully</div>
+        <table>
+            <tr>
+                <td>Data:</td>
+                <td>${JSON.stringify(this.data)}</td>
+            </tr>
+        </table>
     `;
     }
 
